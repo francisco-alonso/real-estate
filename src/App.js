d2912c269ed5e37d662ccf3505f5c7fc5598938d
Francisco <fraloal97@gmail.com>
2024-09-15 15:04:33 +0200

    feat: fetch accounts from metamask

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 
-// Components
-import Navigation from './components/Navigation';
-import Search from './components/Search';
-import Home from './components/Home';
-
-// ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
-
-// Config
-import config from './config.json';
-
 function App() {
+  const [accountsState, setAccountsState] = useState([]);
+
+  useEffect(() => {
+      loadBlockchainData()
+  }, []);
+  
+  const loadBlockchainData = async () => {
+    const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+    
+    setAccountsState(accounts);
+  }
 
   return (
     <div>
-
-      <div className='cards__section'>
-
-        <h3>Welcome to Millow</h3>
-
-      </div>
-
+      <h1>Ethereum Provider</h1>
+      {
+        JSON.stringify(accountsState)
+      }
     </div>
   );
 }
